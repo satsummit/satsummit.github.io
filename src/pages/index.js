824072c317e5bddefd6d9459dd3b8210c09b6fa0
Ricardo Mestre <ricardo@duplos.org>
2022-05-30 17:54:13 +0100

    Include Figure component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
-
-import Layout from '$components/layout';
-
+import { StaticImage } from 'gatsby-plugin-image';
 import { Heading } from '@devseed-ui/typography';
 
+import Layout from '$components/layout';
 import Hug from '$styles/hug';
 import { variableGlsp } from '$styles/variable-utils';
+import { Figcaption, Figure, FigureAttribution } from '$components/figure';
+
 
 const Hero = styled(Hug)`
   padding: ${variableGlsp()};
@@ -20,6 +21,22 @@ const IndexPage = () => {
           <Heading>SatSummit</Heading>
           <p>Hello world.</p>
         </Hero>
+
+        <Figure>
+          <StaticImage
+            src='../images/fig01.jpg'
+            alt='Image alt'
+            placeholder='blurred'
+            layout='constrained'
+            width={1000}
+          />
+          <Figcaption>
+            <FigureAttribution
+              author='Mapbox'
+              url='https://www.flickr.com/photos/mapbox/43082615860/'
+            />
+          </Figcaption>
+        </Figure>
       </main>
     </Layout>
   );
