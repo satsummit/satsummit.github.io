00f428ef34e97c500dace8db8896743a5e991089
Ricardo Mestre <ricardo@duplos.org>
2022-07-06 21:56:51 +0100

    Include tickets page

---
+++ src/pages/tickets.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Layout from '$components/layout';
+
+const TicketsPage = () => {
+  return (
+    <Layout>
+      <main>
+        <p>Hello tickets!</p>
+      </main>
+    </Layout>
+  );
+};
+
+export default TicketsPage;
