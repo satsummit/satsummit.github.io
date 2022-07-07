7f90d4ce1002874e1b4d947c0200b57d1a1a9ffa
Ricardo Mestre <ricardo@duplos.org>
2022-07-07 19:34:14 +0100

    Include page hero components

--- src/styles/page.js
+++ src/styles/page.js
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
 
-import { VarHeading } from './variable-components';
+import { VarHeading, VarLead } from './variable-components';
 import { variableGlsp } from './variable-utils';
 
-import { themeVal } from '@devseed-ui/theme-provider';
+import { media, themeVal } from '@devseed-ui/theme-provider';
 
 import Hug from './hug';
 
@@ -16,13 +16,28 @@ export const PageMainContent = styled.main`
 export const PageMainHero = styled(Hug).attrs({
   as: 'div'
 })`
-  /* border-top: 8px solid ${themeVal('color.secondary-500')}; */
+  border-bottom: 8px solid ${themeVal('color.secondary-500')};
   padding: ${variableGlsp(2, 0)};
+`;
+
+export const PageMainHeroHeadline = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  gap: ${variableGlsp()};
+  grid-column: content-start / content-end;
+
+  ${media.largeUp`
+    grid-column: content-start / content-8;
+  `}
 `;
 
 export const PageMainTitle = styled(VarHeading).attrs({
   as: 'h1',
   size: 'jumbo'
 })`
-  grid-column: content-start / content-end;
+  /* styled-component */
+`;
+
+export const PageLead = styled(VarLead)`
+  color: inherit;
 `;
