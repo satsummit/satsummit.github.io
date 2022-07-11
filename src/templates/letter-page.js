4ecef4fc617a996cb409a0f21007ffe37132c8ed
Daniel da Silva <danielfdsilva@gmail.com>
2022-07-11 12:13:57 +0100

    Make letter pages dynamically sourced from md

---
+++ src/templates/letter-page.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import T from 'prop-types';
+
+import Layout from '$components/layout';
+
+import {
+  PageLead,
+  PageMainContent,
+  PageMainHero,
+  PageMainHeroHeadline,
+  PageMainTitle
+} from '$styles/page';
+
+import { BlockAlpha } from '$styles/blocks';
+import { VarProse } from '$styles/variable-components';
+import { graphql } from 'gatsby';
+
+const LayoutLetter = ({ data }) => {
+  const {
+    html,
+    frontmatter: { title, lead }
+  } = data.markdownRemark;
+
+  return (
+    <Layout>
+      <PageMainContent>
+        <PageMainHero>
+          <PageMainHeroHeadline>
+            <PageMainTitle>{title}</PageMainTitle>
+            {lead && <PageLead>{lead}</PageLead>}
+          </PageMainHeroHeadline>
+        </PageMainHero>
+        <BlockAlpha>
+          <VarProse dangerouslySetInnerHTML={{ __html: html }} />
+        </BlockAlpha>
+      </PageMainContent>
+    </Layout>
+  );
+};
+
+LayoutLetter.propTypes = {
+  data: T.object
+};
+
+export default LayoutLetter;
+
+export const query = graphql`
+  query ($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+      }
+    }
+  }
+`;
