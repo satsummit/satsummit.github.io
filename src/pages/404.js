0402ae968b220115cb5a729af752743158428303
Ricardo Mestre <ricardo@duplos.org>
2022-07-12 15:49:39 +0100

    Restyle and restructure 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,25 +1,42 @@
 import React from 'react';
-import styled from 'styled-components';
-import { DevseedUiThemeProvider, themeVal } from '@devseed-ui/theme-provider';
-import themeOverrides, { GlobalStyles } from '$styles/theme';
 
-import SEO from '$components/seo';
-
-const Page = styled.div`
-  background: ${themeVal('color.primary')};
-  color: ${themeVal('color.base')};
-  min-height: 100vh;
-  display: flex;
-`;
+import Layout from '$components/layout';
+import {
+  PageLead,
+  PageMainContent,
+  PageMainHero,
+  PageMainHeroHeadline,
+  PageMainTitle
+} from '$styles/page';
+import { BlockAlpha } from '$styles/blocks';
+import { VarProse } from '$styles/variable-components';
 
 const UhOh = () => {
   return (
-    <DevseedUiThemeProvider theme={themeOverrides}>
-      {/* eslint-disable-next-line */}
-      <SEO title='Not Found' />
-      <GlobalStyles />
-      <Page>We lost this page</Page>
-    </DevseedUiThemeProvider>
+    <Layout title='Page not found'>
+      <PageMainContent>
+        <PageMainHero>
+          <PageMainHeroHeadline>
+            <PageMainTitle>Page not found</PageMainTitle>
+            <PageLead>That&apos;s a 404 error.</PageLead>
+          </PageMainHeroHeadline>
+        </PageMainHero>
+        <BlockAlpha>
+          <VarProse>
+            <p>
+              We were not able to find the page you are looking for. It may have
+              been archived or removed.
+            </p>
+            <p>
+              If you think this page should be here let us know via{' '}
+              <a href='mailto:info@satsummit.io' title='Send us an email'>
+                info@satsummit.io
+              </a>
+            </p>
+          </VarProse>
+        </BlockAlpha>
+      </PageMainContent>
+    </Layout>
   );
 };
 
