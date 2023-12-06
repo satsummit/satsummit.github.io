029c6d60d56aa7b00cd20508322df7a4a12957a5
Ricardo Mestre <ricardo@duplos.org>
2023-12-06 14:56:16 +0000

    Improve banner display on small screens
    Work in progress.

--- src/components/home/page-hero.tsx
+++ src/components/home/page-hero.tsx
@@ -1,10 +1,5 @@
 import React from 'react';
-import { Box, Flex, Heading, Image, Text, useToken } from '@chakra-ui/react';
-
-import cloudsUrl from '../../images/home-hero-clouds.png';
-import mountainUrl from '../../images/home-hero-mountains.svg';
-import sunUrl from '../../images/home-hero-sun.svg';
-import satelitteUrl from '../../images/home-hero-satellite.png';
+import { Box, Flex, Heading, Image, Text } from '@chakra-ui/react';
 
 import astroUrl from '../../images/banner/banner--astro@2x.png';
 import cloudBigUrl from '../../images/banner/banner--cloud-big@2x.png';
@@ -19,32 +14,27 @@ export default function HomeHero() {
     <Box
       position='relative'
       bg='primary.500'
-      minHeight='36rem'
-      padding='16'
+      minHeight={{ base: '8rem', md: '36rem' }}
+      paddingX={{ base: '4', md: '8' }}
+      paddingY={{ base: '8', md: '16' }}
       display='flex'
-      alignItems='center'
+      flexFlow='column'
+      alignItems={{ base: 'start', md: 'center' }}
     >
       <Flex
-        id='headline'
+        id='banner--headline'
         position='relative'
         zIndex='5'
-        maxW='container.xl'
         height='100%'
-        alignItems={{ md: 'center' }}
-        justifyContent='center'
         flexFlow={{ base: 'column', md: 'row' }}
         gap='4'
-        mx='auto'
       >
         <Heading
           as='h1'
           textTransform='uppercase'
           display='inline-flex'
           flexFlow='column'
-          color={{
-            base: 'white',
-            md: 'base.500'
-          }}
+          color='base.500'
           sx={{
             '> *': {
               lineHeight: '1'
@@ -76,17 +66,11 @@ export default function HomeHero() {
             zIndex='1'
             _before={{
               content: '""',
-              backgroundColor: {
-                base: 'base.500',
-                md: 'secondary.500'
-              },
+              backgroundColor: 'secondary.500',
               position: 'absolute',
               width: '100%',
-              height: { base: '2rem', md: '3.5rem' },
-              bottom: {
-                base: '7px',
-                md: '13px'
-              },
+              height: '0.5em',
+              bottom: '0.10em',
               zIndex: '-1'
             }}
           >
@@ -136,8 +120,9 @@ export default function HomeHero() {
         alt='A plain yellow shape illustrating the sun over a deep blue sky.'
         position='absolute'
         zIndex='1'
-        bottom='19rem'
-        left='calc(50% + 5.5rem)'
+        top={{ base: '0', md: 'auto' }}
+        bottom={{ base: 'auto', md: '19rem' }}
+        left={{ base: '-2rem', md: 'calc(50% + 5.5rem)' }}
         width={{ base: '8rem', md: '10rem', lg: '12rem' }}
       />
 
@@ -146,13 +131,19 @@ export default function HomeHero() {
         alt='A piece of collage illustrating a satellite.'
         position='absolute'
         zIndex='2'
-        top={{ base: '-1rem', md: '-2rem', lg: '-4rem' }}
-        right='calc(50% + 8rem)'
-        width={{ base: '16rem', md: '18rem', lg: '20rem' }}
+        top={{ base: '-2rem', md: '-2rem', lg: '-4rem' }}
+        right={{ base: '2rem', md: 'calc(50% + 8rem)' }}
+        width={{ base: '12rem', md: '18rem', lg: '20rem' }}
         pointerEvents='none'
       />
 
-      <Box id='banner--background' position='absolute' inset='0' overflowX='hidden'>
+      <Box
+        id='banner--background'
+        position='absolute'
+        inset='0'
+        overflowX='hidden'
+        // display={{ base: 'none', md: 'block' }}
+      >
         <Box
           id='banner--clouds'
           position='absolute'
@@ -219,151 +210,6 @@ export default function HomeHero() {
           />
         </Box>
       </Box>
-    </Box>
-  );
-}
-
-const heroBg = [
-  `url('${mountainUrl}') center bottom / auto 35rem no-repeat`,
-  `url('${cloudsUrl}') center bottom / auto 45rem no-repeat`,
-  `url('${sunUrl}') calc(50% + 11rem) calc(50% - 5rem) / 12rem no-repeat`
-].join(', ');
-
-function HomeHero2() {
-  const primary = useToken('colors', 'primary.500');
-
-  return (
-    <Box
-      position='relative'
-      background={{
-        base: primary,
-        // Can't use tokens with this bg notation.
-        md: `${heroBg}, ${primary}`
-      }}
-      height={{ base: 'auto', md: '37rem' }}
-      px={{ base: '8', md: '0' }}
-      pb={{ base: '8', md: '0' }}
-      sx={{
-        svg: {
-          position: 'absolute',
-          bottom: '0'
-        }
-      }}
-    >
-      <Image
-        display={{
-          base: 'none',
-          lg: 'block'
-        }}
-        src={satelitteUrl}
-        position='absolute'
-        top='-2rem'
-        left='calc(50% - 28rem)'
-        width='20rem'
-      />
-      <Flex
-        maxW='container.xl'
-        height='100%'
-        alignItems={{ md: 'center' }}
-        justifyContent='center'
-        flexFlow={{ base: 'column', md: 'row' }}
-        gap='4'
-        mx='auto'
-      >
-        <Heading
-          as='h1'
-          textTransform='uppercase'
-          display='inline-flex'
-          flexFlow='column'
-          color={{
-            base: 'white',
-            md: 'base.500'
-          }}
-          sx={{
-            '> *': {
-              lineHeight: '1'
-            }
-          }}
-        >
-          <Text
-            as='span'
-            alignSelf={{
-              md: 'end'
-            }}
-            fontSize={{ base: '2rem', sm: '2rem', md: '4rem' }}
-          >
-            This is
-          </Text>
-          <Text
-            as='span'
-            fontSize={{ base: '4rem', md: '8rem' }}
-            mt={{ md: '-4' }}
-          >
-            Satsummit
-          </Text>
-          <Text
-            as='span'
-            fontSize={{ base: '4rem', md: '8rem' }}
-            alignSelf='start'
-            mt={{ md: '-4' }}
-            position='relative'
-            zIndex='1'
-            _before={{
-              content: '""',
-              backgroundColor: {
-                base: 'base.500',
-                md: 'secondary.500'
-              },
-              position: 'absolute',
-              width: '100%',
-              height: { base: '2rem', md: '3.5rem' },
-              bottom: {
-                base: '7px',
-                md: '13px'
-              },
-              zIndex: '-1'
-            }}
-          >
-            2024
-          </Text>
-        </Heading>
-        <Box
-          bg='base.500'
-          color='white'
-          position='relative'
-          alignSelf={{ base: 'start', md: 'center' }}
-          p='4'
-          ml={{ base: '7.5rem', md: '-17rem', lg: '-18rem' }}
-          mt={{ base: '-1.75rem', md: '19rem', lg: '4rem' }}
-          order={{ base: '2', lg: '-1' }}
-          _after={{
-            position: 'absolute',
-            content: '""',
-            width: '0',
-            height: '0',
-            right: { base: 'auto', lg: '0' },
-            left: { base: '0', lg: 'auto' },
-            bottom: { base: 'auto', lg: '-1rem' },
-            top: { base: '-1rem', lg: 'auto' },
-            borderTop: { lg: '1rem solid' },
-            borderTopColor: { lg: 'base.500' },
-            borderLeft: { lg: '1.5rem solid transparent' },
-            borderBottom: { base: '1rem solid', lg: 'none' },
-            borderBottomColor: { base: 'base.500', lg: 'none' },
-            borderRight: { base: '1.5rem solid transparent', lg: 'none' }
-          }}
-        >
-          <Heading
-            as='p'
-            textTransform='uppercase'
-            fontSize={{ base: '1.5rem', md: '2rem' }}
-          >
-            May 16 & 17th
-            <br />
-            Washington, D.C.
-          </Heading>
-        </Box>
-      </Flex>
     </Box>
   );
 }
