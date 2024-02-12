f07070db6ad7d0854b5838dd92c218d4eb2cd397
Daniel da Silva <danielfdsilva@gmail.com>
2024-02-12 17:24:48 +0000

    Add responsive menu

--- src/components/page-header.tsx
+++ src/components/page-header.tsx
@@ -1,8 +1,68 @@
 import React from 'react';
-import { Box, Button, Container, Flex } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Container,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+  Flex,
+  Heading,
+  Hide,
+  List,
+  ListItem,
+  ListProps,
+  PlacementWithLogical,
+  Show,
+  Tooltip,
+  useBreakpointValue,
+  useDisclosure
+} from '@chakra-ui/react';
+import { CollecticonHamburgerMenu } from '@devseed-ui/collecticons-chakra';
+
 import Brand from './brand';
+import MenuLink from './menu-link';
+
+import { visuallyDisableProps } from '$utils/utils';
+
+interface NavMenuProps extends ListProps {}
+
+function NavMenu(props: NavMenuProps) {
+  const popoverPosition: PlacementWithLogical | undefined = useBreakpointValue(
+    { base: 'right', md: 'bottom' },
+    { fallback: 'bottom' }
+  );
+
+  return (
+    <List display='flex' columnGap={4} {...props}>
+      <ListItem>
+        <Tooltip label='Coming soon' placement={popoverPosition} hasArrow>
+          <MenuLink to='/schedule' {...visuallyDisableProps()}>
+            Schedule
+          </MenuLink>
+        </Tooltip>
+      </ListItem>
+      <ListItem>
+        <MenuLink to='/tickets'>Tickets</MenuLink>
+      </ListItem>
+      <ListItem>
+        <MenuLink to='/'>Link 3</MenuLink>
+      </ListItem>
+      <ListItem>
+        <MenuLink to='/'>Link 4</MenuLink>
+      </ListItem>
+    </List>
+  );
+}
+
+const menuBreakpoint = 'md';
 
 export default function PageHeader() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <Box
       as='header'
@@ -16,7 +76,16 @@ export default function PageHeader() {
             <Brand variation='negative' />
           </Box>
           <Flex ml='auto'>
-            <Box as='nav'>
+            <Box
+              as='nav'
+              display='flex'
+              flexFlow='row'
+              gap={4}
+              alignItems='center'
+            >
+              <Show above={menuBreakpoint}>
+                <NavMenu />
+              </Show>
               <Button
                 as='a'
                 colorScheme='surface'
@@ -26,10 +95,41 @@ export default function PageHeader() {
               >
                 Call for Speakers
               </Button>
+
+              <Hide above={menuBreakpoint}>
+                <Button
+                  variant='ghost'
+                  colorScheme='surface'
+                  size={{ base: 'sm', lg: 'md' }}
+                  onClick={onOpen}
+                >
+                  <CollecticonHamburgerMenu
+                    title='Open menu drawer'
+                    meaningful
+                  />
+                </Button>
+              </Hide>
             </Box>
           </Flex>
         </Flex>
       </Container>
+      <Hide above={menuBreakpoint}>
+        <Drawer isOpen={isOpen} placement='right' onClose={onClose}>
+          <DrawerOverlay />
+          <DrawerContent>
+            <DrawerCloseButton top={4} right={4} size='sm' />
+            <DrawerHeader display='flex'>
+              <Heading as='span' size='sm'>
+                Menu
+              </Heading>
+            </DrawerHeader>
+
+            <DrawerBody>
+              <NavMenu flexFlow='column' gap={2} />
+            </DrawerBody>
+          </DrawerContent>
+        </Drawer>
+      </Hide>
     </Box>
   );
 }
