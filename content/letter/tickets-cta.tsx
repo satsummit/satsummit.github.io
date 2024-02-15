4a49dc07782cad3586b51d46790183b0008752c9
Ricardo Mestre <ricardo@duplos.org>
2024-02-15 17:11:59 +0000

    Restructure and restyle tickets CTA
    Work in progress.

--- content/letter/tickets-cta.tsx
+++ content/letter/tickets-cta.tsx
@@ -1,15 +1,50 @@
 import React from 'react';
-import { Button } from '@chakra-ui/react';
+import { Box, Button, Heading, Image } from '@chakra-ui/react';
 import SmartLink from '$components/smart-link';
+
+import astroUrl from '../../images/cta/cta--astro@2x.png';
+import cloudSmallUrl from '../../images/cta/cta--cloud-small@2x.png';
+import satelliteUrl from '../../images/cta/cta--satellite@2x.png';
+
+import { CollecticonArrowUpRight } from '@devseed-ui/collecticons-chakra';
 
 export function TicketsCta() {
   return (
-    <Button
-      as={SmartLink}
-      to='https://app.tickettailor.com/events/satsummit2024/1151624'
-      colorScheme='primary'
+    <Box
+      as='section'
+      position='relative'
+      bg='base.50'
+      pointerEvents='none'
+      padding={{ base: '8', lg: '16' }}
     >
-      Buy tickets
-    </Button>
+      <Box
+        pointerEvents='auto'
+        maxW='container.sm'
+        display='flex'
+        flexFlow='column'
+        gap={{ base: '4', md: '8' }}
+      >
+        <Heading as='h2' size='3xl'>
+          Get your ticket
+        </Heading>
+        <p>
+          All tickets include attendance for both May 16 & 17. There are no one
+          day tickets offered at this time. The prices include breakfast and
+          lunch during the conference and light fare at social events. Tickets
+          are nonrefundable.
+        </p>
+        <Box>
+          <Button
+            as={SmartLink}
+            to='https://app.tickettailor.com/events/satsummit2024/1151624'
+            colorScheme='primary'
+            rightIcon={<CollecticonArrowUpRight />}
+            size={{ base: 'md', lg: 'lg' }}
+          >
+            Book ticket
+          </Button>
+        </Box>
+      </Box>
+    </Box>
   );
 }
