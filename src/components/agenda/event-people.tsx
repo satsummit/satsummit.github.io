0081e7bdcb943de2816a3ecc13cb28dde184b91f
Daniel da Silva <danielfdsilva@gmail.com>
2024-04-04 16:42:56 +0100

    Add way to link speakers in MDX

--- src/components/agenda/event-people.tsx
+++ src/components/agenda/event-people.tsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import { List } from '@chakra-ui/react';
 
-import SmartLink from '$components/smart-link';
-
-// const toArray = (v) => {
-//   if (!v) return [];
-//   return Array.isArray(v) ? v : [v];
-// };
+import { LinkedSpeaker } from './linked-speaker';
 
 interface EventPeopleProps {
   list: readonly Queries.PeopleOrVoid[];
@@ -14,30 +9,14 @@ interface EventPeopleProps {
 
 export function EventPeople(props: EventPeopleProps) {
   const { list } = props;
-  // const list = useMemo(() => toArray(props.list), [props.list]);
 
   return (
     <List as='ol' display='flex' flexFlow='row wrap' gap={{ base: 0.5, md: 1 }}>
-      {list.map((person) => {
-        const actualPerson = person as Queries.People;
-        const voidPerson = person as Queries.VoidPeople;
-
-        if (voidPerson.isVoid || actualPerson.group !== 'main') {
-          return (
-            <li key={person.title}>
-              <strong>{person.title}</strong>
-            </li>
-          );
-        } else {
-          return (
-            <li key={actualPerson.slug}>
-              <SmartLink to={`/speakers/${actualPerson.slug}`}>
-                <strong>{actualPerson.title}</strong>
-              </SmartLink>
-            </li>
-          );
-        }
-      })}
+      {list.map((person) => (
+        <li key={(person as Queries.People).slug || person.title}>
+          <LinkedSpeaker name={person.title} />
+        </li>
+      ))}
     </List>
   );
 }
--- src/components/mdx-prose.tsx
+++ src/components/mdx-prose.tsx
@@ -4,11 +4,13 @@ import { Prose } from '@nikolovlazar/chakra-ui-prose';
 
 import { Separator } from './separator';
 import SmartLink from './smart-link';
+import { LinkedSpeaker } from './agenda/linked-speaker';
 
 export function MDXProse(props: React.ComponentProps<typeof MDXProvider>) {
   return (
     <MDXProvider
       components={{
+        Speaker: LinkedSpeaker,
         hr: (props) => <Separator bg='base.200a' size='1' {...props} />,
         /* eslint-disable-next-line  */
         a: (props: any) => (
