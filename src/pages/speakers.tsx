4a1271f869717832d9c3a3635bfa468733cdc266
Daniel da Silva <danielfdsilva@gmail.com>
2024-04-16 16:39:00 +0100

    Add speaker hub page

---
+++ src/pages/speakers.tsx
@@ -0,0 +1,223 @@
+import React, { useMemo } from 'react';
+import { graphql, HeadFC, useStaticQuery } from 'gatsby';
+import { GatsbyImage, getImage, ImageDataLike } from 'gatsby-plugin-image';
+import {
+  Box,
+  Container,
+  Divider,
+  Flex,
+  Heading,
+  List,
+  ListItem,
+  Text,
+  VisuallyHidden
+} from '@chakra-ui/react';
+import { Hug } from '@devseed-ui/hug-chakra';
+
+import PageLayout from '$components/page-layout';
+import Seo from '$components/seo';
+import SmartLink from '$components/smart-link';
+import { ChakraFade } from '$components/reveal';
+
+interface SpeakersPageQuery {
+  allPeople: {
+    nodes: Queries.People[];
+  };
+}
+
+export default function SpeakersPage() {
+  const { allPeople } = useStaticQuery<SpeakersPageQuery>(graphql`
+    query {
+      allPeople(sort: { slug: ASC }) {
+        nodes {
+          id
+          slug
+          title
+          avatar {
+            childImageSharp {
+              gatsbyImageData(width: 640, height: 640, placeholder: BLURRED)
+            }
+          }
+          role
+          company
+          group
+        }
+      }
+    }
+  `);
+
+  const { main, other } = useMemo(
+    () =>
+      allPeople.nodes.reduce<{
+        main: Queries.People[];
+        other: Queries.People[];
+      }>(
+        (acc, node) => {
+          if (node.group === 'main') {
+            acc.main.push(node);
+          } else {
+            acc.other.push(node);
+          }
+          return acc;
+        },
+        { main: [], other: [] }
+      ),
+    [allPeople.nodes]
+  );
+
+  return (
+    <PageLayout>
+      <Box
+        background='primary.500'
+        px={{ base: '4', md: '8' }}
+        py={{ base: '8', lg: '16' }}
+      >
+        <Container
+          maxW='container.xl'
+          color='white'
+          display='flex'
+          flexFlow={{ base: 'column', md: 'row' }}
+          gap={8}
+          p='0'
+        >
+          <Flex flexFlow='column' gap='4'>
+            <Heading size='4xl' as='h1'>
+              Fringe Events
+            </Heading>
+            <Text textStyle='lead.lg' maxW='container.sm'>
+              All the wonderful people presenting at SatSummit.
+            </Text>
+          </Flex>
+        </Container>
+      </Box>
+
+      <Hug py={16}>
+        <Box gridColumn='content-start / content-end'>
+          <VisuallyHidden>
+            <Heading size='xl'>Main speakers</Heading>
+          </VisuallyHidden>
+          <List
+            display='grid'
+            gap={{ base: 4, md: 8 }}
+            gridTemplateColumns={{
+              base: 'repeat(2, 1fr)',
+              md: 'repeat(3, 1fr)',
+              xl: 'repeat(4, 1fr)'
+            }}
+          >
+            <ChakraFade
+              cascade
+              direction='up'
+              triggerOnce
+              duration={500}
+              damping={0.2}
+            >
+              {main.map((speaker) => (
+                <ListItem key={speaker.id} h='100%'>
+                  <Box as='article' bg='base.50' h='100%'>
+                    <SmartLink
+                      to={`/speakers/${speaker.slug}`}
+                      display='flex'
+                      flexFlow='column nowrap'
+                      height='100%'
+                      transition='opacity 0.24s ease-in-out'
+                      color='inherit'
+                      _hover={{
+                        textDecoration: 'none',
+                        opacity: 0.64
+                      }}
+                    >
+                      <Flex
+                        as='header'
+                        flexFlow='column nowrap'
+                        p={{ base: 4, md: 8 }}
+                      >
+                        <Heading as='h3' size='lg'>
+                          {speaker.title}
+                        </Heading>
+                        <Text fontSize={{ base: 'sm', md: 'md' }}>
+                          {speaker.role} at {speaker.company}
+                        </Text>
+                      </Flex>
+                      <Box order={-1}>
+                        <GatsbyImage
+                          image={
+                            getImage(
+                              speaker.avatar as unknown as ImageDataLike
+                            )!
+                          }
+                          alt={`Picture of ${speaker.title}`}
+                          objectFit='contain'
+                        />
+                      </Box>
+                    </SmartLink>
+                  </Box>
+                </ListItem>
+              ))}
+            </ChakraFade>
+          </List>
+        </Box>
+
+        {!!other.length && (
+          <>
+            <Divider
+              gridColumn='content-start / content-end'
+              size='md'
+              borderColor='base.200a'
+            />
+            <Flex
+              gridColumn='content-start / content-end'
+              gap={{ base: 4, md: 8 }}
+              direction='column'
+            >
+              <Heading size='3xl'>Other speakers include</Heading>
+              <List
+                display='grid'
+                gap={{ base: 4, md: 8 }}
+                gridTemplateColumns={{
+                  base: 'repeat(2, 1fr)',
+                  md: 'repeat(3, 1fr)',
+                  xl: 'repeat(4, 1fr)'
+                }}
+              >
+                <ChakraFade
+                  cascade
+                  direction='up'
+                  triggerOnce
+                  duration={500}
+                  damping={0.2}
+                >
+                  {other.map((speaker) => (
+                    <ListItem key={speaker.id}>
+                      <Box as='article'>
+                        <Flex
+                          as='header'
+                          flexFlow='column nowrap'
+                          gap={{ base: 1, md: 2 }}
+                        >
+                          <Heading as='h3' size='2xl'>
+                            {speaker.title}
+                          </Heading>
+                          <Text fontSize={{ base: 'sm', md: 'md' }}>
+                            {speaker.role} at {speaker.company}
+                          </Text>
+                        </Flex>
+                      </Box>
+                    </ListItem>
+                  ))}
+                </ChakraFade>
+              </List>
+            </Flex>
+          </>
+        )}
+      </Hug>
+    </PageLayout>
+  );
+}
+
+export const Head: HeadFC = () => (
+  <Seo
+    title='Speakers'
+    description='All the wonderful people presenting at SatSummit.'
+  />
+);
--- src/templates/people-page.tsx
+++ src/templates/people-page.tsx
@@ -3,18 +3,22 @@ import { graphql, HeadProps, type PageProps } from 'gatsby';
 import { getImage, ImageDataLike } from 'gatsby-plugin-image';
 import {
   Box,
+  Button,
+  Divider,
+  Flex,
   Heading,
   ListItem,
   OrderedList
 } from '@chakra-ui/react';
 import { Hug } from '@devseed-ui/hug-chakra';
+import { CollecticonLayoutGrid3x3 } from '@devseed-ui/collecticons-chakra';
 
 import PageLayout from '$components/page-layout';
 import Seo from '$components/seo';
-
 import { MDXProse } from '$components/mdx-prose';
 import SpeakerHero from '$components/speakers/speaker-hero';
 import { AgendaEvent } from '$components/agenda/event';
+import SmartLink from '$components/smart-link';
 
 interface PeoplePageProps {
   people: Queries.People;
@@ -43,73 +47,114 @@ export default function PeoplerPage(props: PageProps<PeoplePageProps>) {
         image={getImage(avatar as unknown as ImageDataLike)!}
       />
 
-      <Hug pb={16}>
+      <Hug py={16}>
         <MDXProse
           gridColumn={{
             base: 'content-start / content-end',
             md: 'content-2 / content-8',
-            lg: 'content-2 / content-12',
+            lg: 'content-2 / content-12'
           }}
         >
-          <Heading as='h2' size='xl'>About</Heading>
+          <Heading as='h2' size='3xl'>
+            About
+          </Heading>
           {children}
         </MDXProse>
-      </Hug>
 
-      {!!events?.length && (
-        <Hug pb={16}>
-          <Box
-            gridColumn={{
-              base: 'content-start / content-end',
-              md: 'content-2 / content-8',
-              lg: 'content-2 / content-11',
-            }}
-          >
-            <Heading as='h2' size='xl' mb={4}>
-              On the agenda
-            </Heading>
-
-            <Hug
-              as={OrderedList}
-              listStyleType='none'
-              hugGrid={{
-                base: ['content-start', 'content-end'],
-                md: ['content-2', 'content-8'],
-                lg: ['content-2', 'content-11']
+        {!!events?.length && (
+          <>
+            <Divider
+              gridColumn={{
+                base: 'content-start / content-end',
+                md: 'content-2 / content-8',
+                lg: 'content-2 / content-12'
               }}
-              display='flex'
-              flexFlow='column nowrap'
-              ml={0}
+              size='md'
+              borderColor='base.200a'
+            />
+            <Box
+              gridColumn={{
+                base: 'content-start / content-end',
+                md: 'content-2 / content-8',
+                lg: 'content-2 / content-11'
+              }}
             >
-              {events.map(({ event }) => {
-                const eventData = event as Sure<Queries.Event>;
-                return (
-                  <ListItem
-                    key={eventData.cId}
-                    gridColumn='1/-1'
-                    _notFirst={{
-                      borderTop: '4px solid',
-                      borderTopColor: 'base.200a',
-                      pt: { base: 4, md: 8, lg: 10 }
-                    }}
-                  >
-                    <AgendaEvent
-                      startingHLevel={3}
-                      showDate
-                      cId={eventData.cId}
-                      title={eventData.title}
-                      type={eventData.type}
-                      date={eventData.date}
-                      room={eventData.room}
-                      people={eventData.people}
-                    />
-                  </ListItem>
-                );
-              })}
-            </Hug>
-          </Box>
-        </Hug>
-      )}
+              <Heading as='h2' size='3xl' mb={4}>
+                On the agenda
+              </Heading>
+
+              <Hug
+                as={OrderedList}
+                listStyleType='none'
+                hugGrid={{
+                  base: ['content-start', 'content-end'],
+                  md: ['content-2', 'content-8'],
+                  lg: ['content-2', 'content-11']
+                }}
+                display='flex'
+                flexFlow='column nowrap'
+                ml={0}
+              >
+                {events.map(({ event }) => {
+                  const eventData = event as Sure<Queries.Event>;
+                  return (
+                    <ListItem
+                      key={eventData.cId}
+                      gridColumn='1/-1'
+                      _notFirst={{
+                        borderTop: '4px solid',
+                        borderTopColor: 'base.200a',
+                        pt: { base: 4, md: 8, lg: 10 }
+                      }}
+                    >
+                      <AgendaEvent
+                        startingHLevel={3}
+                        showDate
+                        cId={eventData.cId}
+                        title={eventData.title}
+                        type={eventData.type}
+                        date={eventData.date}
+                        room={eventData.room}
+                        people={eventData.people}
+                      />
+                    </ListItem>
+                  );
+                })}
+              </Hug>
+            </Box>
+          </>
+        )}
+
+        <Divider
+          gridColumn={{
+            base: 'content-start / content-end',
+            md: 'content-2 / content-8',
+            lg: 'content-2 / content-12'
+          }}
+          size='sm'
+          borderColor='base.200a'
+        />
+
+        <Flex
+          gridColumn={{
+            base: 'content-start / content-end',
+            md: 'content-2 / content-8',
+            lg: 'content-2 / content-12'
+          }}
+        >
+          <Button
+            as={SmartLink}
+            noLinkStyles
+            to='/speakers'
+            variant='solid'
+            colorScheme='primary'
+            size={{ base: 'md', md: 'lg' }}
+            leftIcon={<CollecticonLayoutGrid3x3 />}
+          >
+            View all speakers
+          </Button>
+        </Flex>
+      </Hug>
     </PageLayout>
   );
 }
