00f485f4b7842b082e90a7c5e60e7bc906eccbf3
Daniel da Silva <danielfdsilva@gmail.com>
2024-07-04 18:30:36 +0100

    Add preliminary edition content

---
+++ src/pages/2024-washington/index.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import { graphql, type HeadFC, type PageProps } from 'gatsby';
+import { Flex, Text, Heading } from '@chakra-ui/react';
+
+import PageLayout from '$components/page-layout';
+import HomeHero from '$components/home/page-hero';
+import { Fold, FoldProse } from '$components/fold';
+import Seo from '$components/seo';
+
+export default function IndexPage(props: PageProps) {
+  return (
+    <PageLayout pageProps={props}>
+      <HomeHero />
+      <Flex
+        flexFlow='column'
+        gap='8'
+        py='12'
+        px='4'
+        position='relative'
+        zIndex='30'
+      >
+        <Fold>
+          <FoldProse
+            display='flex'
+            flexFlow='column'
+            gridColumn='1/-1'
+            gap={{ base: '4', lg: '8' }}
+          >
+            <Heading size='2xl'>Washington edition</Heading>
+            <Text>This is the washington edition of the SatSummit.</Text>
+          </FoldProse>
+        </Fold>
+      </Flex>
+    </PageLayout>
+  );
+}
+
+export const pageQuery = graphql`
+  query ($editionCId: String) {
+    ...SponsorsData
+  }
+`;
+
+export const Head: HeadFC = () => <Seo title='Welcome' />;
