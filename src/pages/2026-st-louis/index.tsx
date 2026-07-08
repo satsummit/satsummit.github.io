471d213218f116585944f4c7244e81fa6e7068ec
Daniel da Silva <danielfdsilva@gmail.com>
2026-07-08 08:57:21 +0100

    Add St Louis homepage content

--- src/pages/2026-st-louis/index.tsx
+++ src/pages/2026-st-louis/index.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import { graphql, HeadProps, type PageProps } from 'gatsby';
+import { StaticImage } from 'gatsby-plugin-image';
 
 import PageLayout from '$components/page-layout';
 import Seo from '$components/seo';
 
 import HomeHero from './_hero';
-import { Stack, Text } from '@chakra-ui/react';
-import SmartLink from '$components/smart-link';
-
-const A = (props: React.ComponentProps<typeof SmartLink>) => (
-  <SmartLink {...props} fontWeight='bold' color='primary.500' />
-);
+import { Button, Flex, Heading, Separator, Text } from '@chakra-ui/react';
+import { Fold, FoldMedia, FoldProse } from '$components/fold';
+import { ChakraFade } from '$components/reveal';
+import { CollecticonDownload2 } from '@devseed-ui/collecticons-chakra';
 
 export default function IndexPage(
   props: PageProps<Queries.Home2026StLouisQuery>
@@ -18,35 +17,149 @@ export default function IndexPage(
   return (
     <PageLayout pageProps={props}>
       <HomeHero />
-      <Stack maxW='4xl' mx='auto' px={4} py={12} fontSize='lg' gap={6}>
-        <Text>
-          Join us in St. Louis this November 18-19 for the next edition of
-          SatSummit — hosted by{' '}
-          <A to='https://www.commonspace.world/'>Common Space</A>,{' '}
-          <A to='https://taylorgeospatial.org/'>Taylor Geospatial</A>, and{' '}
-          <A to='https://www.developmentseed.org/'>Development Seed</A>. Known
-          for bringing together leaders across Earth observation, geospatial
-          technology, science, design, and decision-making, SatSummit has built
-          a reputation for thoughtful conversations, ambitious ideas, and a
-          community that cares deeply about the future of our planet
-        </Text>
-        <Text>
-          We&apos;re excited to bring the event to St. Louis for the first time
-          and can&apos;t wait to share more soon. Sign up for the newsletter and{' '}
-          <A to='https://www.linkedin.com/showcase/satsummit/'>
-            follow us on LinkedIn
-          </A>{' '}
-          to stay up to date on speakers, programming, and announcements.
-        </Text>
-        <Text>
-          Interested in supporting the event? Sponsorship opportunities are now
-          available — Check out the <A to='/2026-st-louis-sponsor-prospectus.pdf'>
-            prospectus
-          </A> and{' '}
-          <A to='mailto:info@satsummit.io'>get in touch</A> to learn more about
-          partnering with SatSummit St. Louis.
-        </Text>
-      </Stack>
+
+      <Flex
+        flexFlow='column'
+        gap='8'
+        py='12'
+        px='4'
+        position='relative'
+        zIndex='30'
+      >
+        <Fold>
+          <ChakraFade
+            direction='up'
+            triggerOnce
+            gridColumn={{ base: '1/-1', lg: '1/ span 6' }}
+            alignSelf='end'
+            delay={200}
+          >
+            <FoldMedia>
+              <StaticImage
+                src='./home-vibe-1.png'
+                alt='Four people sitting in line with the second one talking to a microphone'
+              />
+            </FoldMedia>
+          </ChakraFade>
+          <ChakraFade
+            direction='up'
+            triggerOnce
+            gridColumn={{ base: '1/-1', lg: '7/ span 6' }}
+          >
+            <FoldProse
+              display='flex'
+              flexFlow='column'
+              gap={{ base: '4', lg: '8' }}
+              mt={0}
+            >
+              <Heading size='2xl'>Save the date</Heading>
+              <Text>
+                <strong>SatSummit</strong> convenes leaders in the satellite
+                industry and experts in global development for 2 days of
+                presentations and in-depth conversations on solving the
+                world&apos;s most critical development challenges with satellite
+                data.
+              </Text>
+              <Separator borderColor='basi.200a' size='md' />
+              <Heading size='2xl'>Stay Tuned</Heading>
+              <Text>
+                From climate change to population growth to natural resource
+                availability, earth observation data offers insights into
+                today&apos;s biggest global issues.
+              </Text>
+              <Text>
+                Subscribe to the newsletter for more information on the{' '}
+                <strong>SatSummit 2026</strong>!
+              </Text>
+              <Button
+                as='a'
+                // @ts-expect-error types from "a" not propagating properly
+                href='#newsletter-fold'
+                variant='solid'
+                colorPalette='primary'
+                alignSelf='start'
+                size={{ base: 'md', lg: 'lg' }}
+              >
+                Get the newsletter
+              </Button>
+            </FoldProse>
+          </ChakraFade>
+        </Fold>
+        <Fold>
+          <ChakraFade
+            direction='up'
+            triggerOnce
+            gridColumn={{ base: '1/-1', md: 'span 4', lg: '1/ span 4' }}
+          >
+            <FoldMedia>
+              <StaticImage
+                src='./home-vibe-2.png'
+                alt='Group of people happily talking to each other'
+              />
+            </FoldMedia>
+          </ChakraFade>
+          <ChakraFade
+            direction='up'
+            triggerOnce
+            gridColumn={{ base: '1/-1', md: 'span 4', lg: '5/ span 8' }}
+            delay={200}
+          >
+            <FoldMedia>
+              <StaticImage
+                src='./home-vibe-3.png'
+                alt='Person on a stage talking to an audience seen from the audience perspective'
+              />
+            </FoldMedia>
+          </ChakraFade>
+        </Fold>
+        <Fold>
+          <ChakraFade
+            direction='up'
+            triggerOnce
+            gridColumn={{ base: '1/-1', lg: '1/ span 6' }}
+          >
+            <FoldProse
+              display='flex'
+              flexFlow='column'
+              gap={{ base: '4', lg: '8' }}
+              mt={0}
+            >
+              <Heading size='2xl'>Become a sponsor</Heading>
+              <Text>
+                We&apos;re excited to partner with thought and industry leaders
+                in the satellite and development communities, and through their
+                sponsorship and support of <strong>SatSummit</strong>, we are
+                solving real-world and global development challenges.
+              </Text>
+              <Button
+                as='a'
+                // @ts-expect-error types from "a" not propagating properly
+                href='https://satsummit.io/2026-st-louis-sponsor-prospectus.pdf'
+                variant='solid'
+                colorPalette='primary'
+                alignSelf='start'
+                size={{ base: 'md', lg: 'lg' }}
+              >
+                Download the kit <CollecticonDownload2 />
+              </Button>
+            </FoldProse>
+          </ChakraFade>
+          <ChakraFade
+            direction='up'
+            triggerOnce
+            gridColumn={{ base: '1/-1', lg: '7/ span 6' }}
+            alignSelf='end'
+            delay={200}
+          >
+            <FoldMedia>
+              <StaticImage
+                src='./home-vibe-4.png'
+                alt='Several people getting food at a conference buffet line'
+              />
+            </FoldMedia>
+          </ChakraFade>
+        </Fold>
+      </Flex>
     </PageLayout>
   );
 }
