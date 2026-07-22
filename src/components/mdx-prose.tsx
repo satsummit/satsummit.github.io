cb564e641dcd2f3116dcee2e14ee499c38c9c3fb
Daniel da Silva <danielfdsilva@gmail.com>
2026-07-22 10:45:13 +0100

    Update St Louis practical info

--- src/components/mdx-prose.tsx
+++ src/components/mdx-prose.tsx
@@ -15,7 +15,18 @@ import { MDXComponents } from 'mdx/types';
 
 function Prose({ children, ...props }: StackProps) {
   return (
-    <Stack as='article' gap={4} alignItems='start' {...props}>
+    <Stack
+      as='article'
+      gap={4}
+      alignItems='start'
+      css={{
+        '& span:has(.gatsby-resp-image-wrapper)': {
+          w: '100%',
+          display: 'block'
+        }
+      }}
+      {...props}
+    >
       {children}
     </Stack>
   );
@@ -32,9 +43,15 @@ const mdComponents: Readonly<MDXComponents> = {
   h3: (props) => (
     <Heading as='h3' mt='0.5em' size={{ base: 'xl', lg: '2xl' }} {...props} />
   ),
-  h4: (props) => <Heading as='h4' mt='0.5em' size={{ base: 'lg', lg: 'xl' }} {...props} />,
-  h5: (props) => <Heading as='h5' mt='0.5em' size={{ base: 'md', lg: 'lg' }} {...props} />,
-  h6: (props) => <Heading as='h6' mt='0.5em' size={{ base: 'sm', lg: 'md' }} {...props} />,
+  h4: (props) => (
+    <Heading as='h4' mt='0.5em' size={{ base: 'lg', lg: 'xl' }} {...props} />
+  ),
+  h5: (props) => (
+    <Heading as='h5' mt='0.5em' size={{ base: 'md', lg: 'lg' }} {...props} />
+  ),
+  h6: (props) => (
+    <Heading as='h6' mt='0.5em' size={{ base: 'sm', lg: 'md' }} {...props} />
+  ),
   ul: (props) => <List.Root pl={6} {...props} />,
   ol: (props) => <List.Root as='ol' pl={6} {...props} />,
   li: (props) => <List.Item _marker={{ color: 'basi.500' }} {...props} />,
@@ -50,7 +67,7 @@ const mdComponents: Readonly<MDXComponents> = {
   )
 };
 
-export function MDXProse(props: FlexProps) {
+export function MDXProse(props: StackProps) {
   const { children, ...rest } = props;
 
   return (
